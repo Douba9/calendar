639f0e74b9300aa05055a6401a4b71e8c2bd737f
marius <marius.mestron@epitech.eu>
2022-09-22 14:24:08 +0200

    add step 1

---
+++ tester_step1.php
@@ -0,0 +1,14 @@
+<?php
+
+function display_event(Array $event)
+{
+    echo "The \"".$event['name']."\" event will take place on ".date('d-m-Y', strtotime($event['date']))." in ".$event['location']."\n";
+}
+
+$event = [
+    'name' => 'RDV Client Smith',
+    'date' => '20191231',
+    'location' => 'Nantes'
+];
+
+display_event($event);
